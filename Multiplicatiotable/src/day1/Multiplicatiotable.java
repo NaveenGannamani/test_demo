833a663653c75f18bebdfd15b1e6d9ea2ea5f937
Naveen <Naveen@DESKTOP-IR9KMPD>
2022-02-28 19:46:17 +0530

    pull and commit

---
+++ Multiplicatiotable/src/day1/Multiplicatiotable.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Multiplicatiotable {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}
+
---
+++ Multiplicatiotable/src/module-info.java
@@ -0,0 +1,2 @@
+module Multiplicatiotable {
+}
